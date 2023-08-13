097ab0f2155bf7b8e8bb8797a3e22724d17c747c
Pashttett <pavlo3637@gmail.com>
2023-08-13 15:20:37 +0300

    goit-react-hw-03-image-finder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,66 +19,53 @@ class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchQuery !== this.state.searchQuery) {
-      this.setState({ page: 1, images: [], hasMoreImages: false }, () => {
-        this.fetchInitialImages();
-      });
+      this.fetchImages();
     }
   }
 
-  handleFormSubmit = query => {
-    if (!query) {
-      return;
-    }
+handleFormSubmit = query => {
+  if (!query) {
+    return;
+  }
 
-    this.setState({ searchQuery: query }, () => {
-      this.fetchInitialImages();
+  this.setState({ searchQuery: query, images: [], page: 1 }, () => {
+    this.fetchImages();
+  });
+};
+
+
+handleLoadMore = () => {
+  this.setState(prevState => ({
+    page: prevState.page + 1,
+  }), () => {
+    this.fetchImages();
+  });
+};
+
+fetchImages = () => {
+  const { searchQuery, page } = this.state;
+
+  this.setState({ isLoading: true });
+
+  const fetchFunction = page === 1 ? fetchInitialImages : fetchMoreImages;
+
+  fetchFunction(searchQuery, page)
+    .then(imagesWithLargerSizes => {
+      this.setState({
+        images: page === 1 ? imagesWithLargerSizes : [...this.state.images, ...imagesWithLargerSizes],
+        hasMoreImages: imagesWithLargerSizes.length === 12,
+      });
+    })
+    .catch(error => console.error('Error fetching images:', error))
+    .finally(() => {
+      this.setState({ isLoading: false });
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
     });
-  };
+};
 
-  fetchInitialImages = () => {
-    const { searchQuery, page } = this.state;
-
-    this.setState({ isLoading: true });
-
-    fetchInitialImages(searchQuery, page)
-      .then(imagesWithLargerSizes => {
-        this.setState({
-          images: imagesWithLargerSizes,
-          hasMoreImages: imagesWithLargerSizes.length === 12,
-        });
-      })
-      .catch(error => console.error('Error fetching images:', error))
-      .finally(() => {
-        this.setState({ isLoading: false });
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: 'smooth',
-        });
-      });
-  };
-
-  fetchMoreImages = () => {
-    const { searchQuery, page } = this.state;
-
-    this.setState({ isLoading: true });
-
-    fetchMoreImages(searchQuery, page + 1)
-      .then(newImagesWithLargerSizes => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...newImagesWithLargerSizes],
-          page: prevState.page + 1,
-          hasMoreImages: newImagesWithLargerSizes.length === 12,
-        }));
-      })
-      .catch(error => console.error('Error fetching images:', error))
-      .finally(() => {
-        this.setState({ isLoading: false });
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: 'smooth',
-        });
-      });
-  };
 
   toggleModal = (imageURL = '') => {
     this.setState(prevState => ({
@@ -94,7 +81,9 @@ class App extends Component {
       <div className="App">
         <Searchbar onSubmit={this.handleFormSubmit} />
         <ImageGallery images={images} toggleModal={this.toggleModal} />
-        {hasMoreImages && <Button onClick={this.fetchMoreImages} />}
+        {hasMoreImages && (
+          <Button onClick={this.handleLoadMore} />
+        )}
         {isLoading && <Loader />}
         {showModal && (
           <Modal onClose={this.toggleModal} largeImageURL={selectedImage} />
